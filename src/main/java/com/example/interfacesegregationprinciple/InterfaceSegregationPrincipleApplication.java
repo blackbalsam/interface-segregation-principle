b2f6ae962dfc97b2a3c00f1b839981a03b2c9c76
Gatis Jermacans <gatis.jermacans@gmail.com>
2020-04-11 10:13:59 +0300

    Initial commit

---
+++ src/main/java/com/example/interfacesegregationprinciple/InterfaceSegregationPrincipleApplication.java
@@ -0,0 +1,13 @@
+package com.example.interfacesegregationprinciple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterfaceSegregationPrincipleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterfaceSegregationPrincipleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/interfacesegregationprinciple/InterfaceSegregationPrincipleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.interfacesegregationprinciple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterfaceSegregationPrincipleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
