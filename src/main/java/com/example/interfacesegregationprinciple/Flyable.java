b132edac288136a826626b368985b62da15053c7
Gatis Jermacans <gatis.jermacans@gmail.com>
2020-04-11 10:37:07 +0300

    Initial project commit

---
+++ src/main/java/com/example/interfacesegregationprinciple/Flyable.java
@@ -0,0 +1,5 @@
+package com.example.interfacesegregationprinciple;
+
+public interface Flyable {
+    void fly();
+}
---
+++ src/main/java/com/example/interfacesegregationprinciple/Movable.java
@@ -0,0 +1,5 @@
+package com.example.interfacesegregationprinciple;
+
+public interface Movable {
+    void move();
+}
